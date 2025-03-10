542e2a341e41c93ce3edaf83b97d8c11fe482056
hadX30 <hadeelalbrak@gmail.com>
2025-03-10 19:07:57 +0300

    first commit

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Lab3
 # Lab4
 # Lab5
+# Lab6
--- apps/bookmodule/views.py
+++ apps/bookmodule/views.py
@@ -17,6 +17,12 @@ def viewbook(request, bookId):
     context = {'book':targetBook} # book is the variable name accessible by the template
     return render(request, 'bookmodules/show.html', context)
 
+def __getBooksList():
+    book1 = {'id':12344321, 'title':'Continuous Delivery', 'author':'J.Humble and D. Farley'}
+    book2 = {'id':56788765,'title':'Reversing: Secrets of Reverse Engineering', 'author':'E. Eilam'}
+    book3 = {'id':43211234, 'title':'The Hundred-Page Machine Learning Book', 'author':'Andriy Burkov'}
+    return [book1, book2, book3]
+
 def index(request):
     return render(request, "bookmodules/index.html")
 def list_books(request):
@@ -33,3 +39,21 @@ def listing(request):
     return render(request, 'bookmodules/listing.html') 
 def tables(request):
     return render(request, 'bookmodules/tables.html') 
+def search(request):
+    if request.method == "POST":
+        string = request.POST.get('keyword').lower()
+        isTitle = request.POST.get('option1')
+        isAuthor = request.POST.get('option2')
+        # now filter
+        books = __getBooksList()
+        newBooks = []
+        for item in books:
+            contained = False
+            if isTitle and string in item['title'].lower(): contained = True
+            if not contained and isAuthor and string in item['author'].lower():contained = True
+            
+            if contained: newBooks.append(item)
+        return render(request, 'bookmodules/bookList.html', {'books':newBooks})
+
+    return render(request, 'bookmodules/search.html') 
+
